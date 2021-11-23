83289d3be1609d14c95fe3b9c7178b49c9a0467d
jeenu-chacko <jeenu.chacko7@gmail.com>
2021-11-24 02:35:28 +0530

    feat: add avatar svg, badges component

---
+++ src/assets/badges-icon/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export function BadgeOne() {
+  return (
+    <img src="https://img.icons8.com/external-prettycons-lineal-prettycons/49/000000/external-badge-shopping-prettycons-lineal-prettycons.png" />
+  );
+}
+
+export function BadgeTwo() {
+  return (
+    <img src="https://img.icons8.com/external-prettycons-lineal-color-prettycons/49/000000/external-badge-technology-prettycons-lineal-color-prettycons.png" />
+  );
+}
+
+export function BadgeThree() {
+  return (
+    <img src="https://img.icons8.com/external-prettycons-flat-prettycons/47/000000/external-badge-technology-prettycons-flat-prettycons.png" />
+  );
+}
