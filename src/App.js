cc869442af0c425d7d7091259cc8496e0a3de635
R Aravind <aravind.ar786@gmail.com>
2021-11-23 20:40:34 +0530

    feat: added react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import react from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
