cec425d47cff9c2876fdd47416c9f94bc985f30e
jeenu-chacko <jeenu.chacko7@gmail.com>
2021-11-23 21:30:22 +0530

    feat: completed bottomNavigation

--- src/component/bottomNavigation.js
+++ src/component/bottomNavigation.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { IconName } from 'react-icons/ai';
+import { IoFastFoodOutline } from 'react-icons/io5';
+import { BiHomeAlt, BiPlusCircle, BiLocationPlus } from 'react-icons/bi';
 
 function bottomNavigation() {
-  return <div></div>;
+  return (
+    <div className="flex fixed items-center justify-evenly w-full mt-2 bg-gray-100 h-14 ">
+      <IoFastFoodOutline className=" text-4xl   text-gray-600" />
+      <BiHomeAlt className=" text-4xl  cursor-pointer  text-gray-600" />
+      <BiPlusCircle className=" text-4xl  cursor-pointer text-gray-600" />
+      <BiLocationPlus className=" text-4xl cursor-pointer text-gray-600" />
+    </div>
+  );
 }
 
 export default bottomNavigation;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,3 @@
-import react from 'react';
 import { FiUser } from 'react-icons/fi';
 import {
   BiCurrentLocation,
@@ -7,76 +6,87 @@ import {
   BiSpa,
   BiCheckCircle,
   BiChevronRight,
+  BiHomeAlt,
+  BiPlusCircle,
+  BiLocationPlus,
 } from 'react-icons/bi';
+import { IoFastFoodOutline } from 'react-icons/io5';
 
 const Home = () => {
   return (
-    <div className="w-screen h-screen p-8">
-      <div className="flex items-center justify-between w-full h-12">
-        <div className="flex items-center h-full px-6 py-4 rounded-md bg-product-blue text-product-dark">
-          <BiCurrentLocation />
-          <h2 className="ml-4 font-semibold ">Chengannur, India</h2>
+    <div className="h-screen">
+      <div className="w-screen p-8">
+        <div className="flex items-center justify-between w-full h-12">
+          <div className="flex items-center h-full px-6 py-4 rounded-md bg-product-blue text-product-dark">
+            <BiCurrentLocation />
+            <h2 className="ml-4 font-semibold ">Chengannur, India</h2>
+          </div>
+          <div className="h-full px-6 py-4 rounded-md bg-product-yellow">
+            <FiUser />
+          </div>
         </div>
-        <div className="h-full px-6 py-4 rounded-md bg-product-yellow">
-          <FiUser />
+        <div className="relative flex items-center mt-8">
+          <BiSearch className="absolute text-xl left-4 text-product-dark" />
+          <input
+            type="text"
+            className="w-full p-4 pl-12 border rounded-md bg-product-blue h-14 focus:outline-none text-product-dark placeholder-product-dark"
+            placeholder="Explore"
+          />
         </div>
-      </div>
-      <div className="relative flex items-center mt-8">
-        <BiSearch className="absolute text-xl left-4 text-product-dark" />
-        <input
-          type="text"
-          className="w-full p-4 pl-12 border rounded-md bg-product-blue h-14 focus:outline-none text-product-dark placeholder-product-dark"
-          placeholder="Explore"
-        />
-      </div>
-      <div className="mt-8">
-        <h2 className="w-full text-xl text-center text-product-dark">
-          <span className="text-4xl font-semibold text-product-blue">
-            Chengannur {' '}
-          </span>
-          is a Municipality in the Alappuzha district of Kerala State, India. It
-          is located in the extreme eastern part of the Alappuzha district, on
-          the banks of Pamba River.
-        </h2>
-      </div>
-      <div className="px-4 mt-8">
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center justify-center">
-            <div className="flex items-center justify-center text-2xl rounded-md w-14 h-14 bg-product-pink text-product-light">
-              <BiNews />
+        <div className="mt-8">
+          <h2 className="w-full text-xl text-center text-product-dark">
+            <span className="text-4xl font-semibold text-product-blue">
+              Chengannur{' '}
+            </span>
+            is a Municipality in the Alappuzha district of Kerala State, India.
+            It is located in the extreme eastern part of the Alappuzha district,
+            on the banks of Pamba River.
+          </h2>
+        </div>
+        <div className="px-4 mt-8">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center text-2xl rounded-md w-14 h-14 bg-product-pink text-product-light">
+                <BiNews />
+              </div>
+
+              <h2 className="ml-8 text-xl">What's happening</h2>
             </div>
 
-            <h2 className="ml-8 text-xl">What's happening</h2>
+            <BiChevronRight className="ml-4 text-4xl text-product-pink" />
           </div>
-
-          <BiChevronRight className="ml-4 text-4xl text-product-pink" />
         </div>
-      </div>
-      <div className="px-4 mt-6">
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center justify-center">
-            <div className="flex items-center justify-center text-2xl rounded-md w-14 h-14 bg-product-blue text-product-light">
-              <BiCheckCircle />
+        <div className="px-4 mt-6">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center text-2xl rounded-md w-14 h-14 bg-product-blue text-product-light">
+                <BiCheckCircle />
+              </div>
+
+              <h2 className="ml-8 text-xl">Do's and Don'ts</h2>
             </div>
-
-            <h2 className="ml-8 text-xl">Do's and Don'ts</h2>
+            <BiChevronRight className="ml-4 text-4xl text-product-blue" />
           </div>
-          <BiChevronRight className="ml-4 text-4xl text-product-blue" />
         </div>
-      </div>
-      <div className="px-4 mt-6">
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center justify-center">
-            <div className="flex items-center justify-center text-2xl rounded-md w-14 h-14 bg-product-yellow text-product-light">
-              <BiSpa />
+        <div className="px-4 mt-6">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center text-2xl rounded-md w-14 h-14 bg-product-yellow text-product-light">
+                <BiSpa />
+              </div>
+
+              <h2 className="ml-8 text-xl">Culture</h2>
             </div>
-
-            <h2 className="ml-8 text-xl">Culture</h2>
+            <BiChevronRight className="ml-4 text-4xl text-product-yellow" />
           </div>
-          <BiChevronRight className="ml-4 text-4xl text-product-yellow" />
         </div>
       </div>
-
+      <div className="flex fixed fixed inset-x-0 bottom-0 z-10 items-center justify-evenly w-full mt-2 bg-gray-50 h-14 ">
+        <IoFastFoodOutline className=" text-4xl   text-gray-600" />
+        <BiHomeAlt className=" text-4xl  cursor-pointer  text-gray-600" />
+        <BiPlusCircle className=" text-4xl   cursor-pointer text-gray-600" />
+        <BiLocationPlus className=" text-4xl   cursor-pointer text-gray-600" />
+      </div>
     </div>
   );
 };
