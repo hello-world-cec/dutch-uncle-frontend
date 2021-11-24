477cdb67b393f4fad2597c3e618856b3a1ff18ad
R Aravind <aravind.ar786@gmail.com>
2021-11-24 13:08:08 +0530

    feat: BottomNavigation now implements dynamically - loads links and paths from a list of links

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -1,13 +1,45 @@
 import { IoFastFoodOutline } from 'react-icons/io5';
 import { BiHomeAlt, BiPlusCircle, BiLocationPlus } from 'react-icons/bi';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function BottomNavigation() {
+  const Links = [
+    {
+      path: '/',
+      icon: BiHomeAlt,
+    },
+    {
+      path: '/services',
+      icon: BiPlusCircle,
+    },
+    {
+      path: '/restaurants',
+      icon: IoFastFoodOutline,
+    },
+    {
+      path: '/places',
+      icon: BiLocationPlus,
+    },
+  ];
+
+  const location = useLocation();
   return (
     <div className="fixed inset-x-0 bottom-0 flex items-center w-full h-16 mt-2 bg-gray-100 justify-evenly ">
-      <BiHomeAlt className="text-2xl cursor-pointer text-product-dark" />
-      <BiPlusCircle className="text-2xl text-gray-400 cursor-pointer " />
-      <IoFastFoodOutline className="text-2xl text-gray-400 " />
-      <BiLocationPlus className="text-2xl text-gray-400 cursor-pointer " />
+      {Links &&
+        Links.map((link) => {
+          return (
+            <Link to={link.path}>
+              <link.icon
+                className={`text-2xl cursor-pointer ${
+                  location.pathname === link.path
+                    ? 'text-product-dark'
+                    : 'text-gray-400'
+                }`}
+              />
+            </Link>
+          );
+        })}
     </div>
   );
 }
