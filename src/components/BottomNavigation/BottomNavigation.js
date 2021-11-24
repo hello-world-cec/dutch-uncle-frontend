65202feaeb3d0c12455346911bc5b160ac09f0d7
R Aravind <aravind.ar786@gmail.com>
2021-11-24 10:59:34 +0530

    feat: improved BottomNavigation icon placement and styling

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -3,11 +3,11 @@ import { BiHomeAlt, BiPlusCircle, BiLocationPlus } from 'react-icons/bi';
 
 function BottomNavigation() {
   return (
-    <div className="flex fixed inset-x-0 bottom-0 items-center justify-evenly w-full mt-2 bg-gray-100 h-14 ">
-      <IoFastFoodOutline className=" text-4xl   text-gray-600" />
-      <BiHomeAlt className=" text-4xl  cursor-pointer  text-gray-600" />
-      <BiPlusCircle className=" text-4xl  cursor-pointer text-gray-600" />
-      <BiLocationPlus className=" text-4xl cursor-pointer text-gray-600" />
+    <div className="fixed inset-x-0 bottom-0 flex items-center w-full h-16 mt-2 bg-gray-100 justify-evenly ">
+      <BiHomeAlt className="text-2xl cursor-pointer text-product-dark" />
+      <BiPlusCircle className="text-2xl text-gray-400 cursor-pointer " />
+      <IoFastFoodOutline className="text-2xl text-gray-400 " />
+      <BiLocationPlus className="text-2xl text-gray-400 cursor-pointer " />
     </div>
   );
 }
