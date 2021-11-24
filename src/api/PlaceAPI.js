de7e40127b78c8e7338d62615bd919622787655b
R Aravind <aravind.ar786@gmail.com>
2021-11-24 18:36:35 +0530

    feat: added methods to get news, places, restaurants, services and dos and donts

--- src/api/PlaceAPI.js
+++ src/api/PlaceAPI.js
@@ -17,4 +17,55 @@ const refreshLocationDetails = async () => {
   localStorage.setItem('data', data);
 };
 
-export { setCurrentLocation, refreshLocationDetails };
+const getNews = () => {
+  refreshLocationDetails();
+  const json = localStorage.getItem('data');
+  const data = JSON.parse(json);
+  return data.news;
+};
+
+const getCulture = () => {
+  refreshLocationDetails();
+  const json = localStorage.getItem('data');
+  const data = JSON.parse(json);
+  return data.culture;
+};
+
+const getDosAndDonts = () => {
+  refreshLocationDetails();
+  const json = localStorage.getItem('data');
+  const data = JSON.parse(json);
+  return data.dos;
+};
+
+const getPlaces = () => {
+  refreshLocationDetails();
+  const json = localStorage.getItem('data');
+  const data = JSON.parse(json);
+  return data.landmarks;
+};
+
+const getRestaurants = () => {
+  refreshLocationDetails();
+  const json = localStorage.getItem('data');
+  const data = JSON.parse(json);
+  return data.restaurants;
+};
+
+const getServices = () => {
+  refreshLocationDetails();
+  const json = localStorage.getItem('data');
+  const data = JSON.parse(json);
+  return data.services;
+};
+
+export {
+  setCurrentLocation,
+  refreshLocationDetails,
+  getNews,
+  getCulture,
+  getDosAndDonts,
+  getPlaces,
+  getRestaurants,
+  getServices,
+};
